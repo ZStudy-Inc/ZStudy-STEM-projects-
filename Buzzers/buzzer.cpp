c1c1d84a39e867d5750191a271fb7e74b1b630f3
TheskeptcDC <144281616+TheskeptcDC@users.noreply.github.com>
2024-08-03 12:41:44 -0700

    time incremental deep

--- Buzzers/buzzer.cpp
+++ Buzzers/buzzer.cpp
@@ -1,6 +1,6 @@
 // using the buzzer (piazo), connected to pin 2 of the micro controller 
 // pin 2 named palse_beep 
-// the buzzer beeps every after 1 second 
+// the buzzer beeps every after 1 second ,then2 seconds and then 3 seconds 
 // image palse_beep2.png      --for circit design 
 #define palse_beep 7
 
@@ -13,12 +13,24 @@ void loop(){
     delay(1000);
     digitalWrite(palse_beep,0);
     delay(1000);
+    digitalWrite(palse_beep,1);
+    delay(1000);
+    digitalWrite(palse_beep,0);
+    delay(1000);
     // longer time beeb
     digitalWrite(palse_beep,1);
     delay(2000);
     digitalWrite(palse_beep,0);
     delay(2000);
+    digitalWrite(palse_beep,1);
+    delay(2000);
+    digitalWrite(palse_beep,0);
+    delay(2000);
     // even longer beep
+    digitalWrite(palse_beep,1);
+    delay(3000);
+    digitalWrite(palse_beep,0);
+    delay(3000);
     digitalWrite(palse_beep,1);
     delay(3000);
     digitalWrite(palse_beep,0);
